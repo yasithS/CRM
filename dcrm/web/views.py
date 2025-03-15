c8704ceefef909257b9c63e907acf4bc30767a34
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2025-03-15 09:15:42 +0530

    feat: signup method implementes

--- dcrm/web/views.py
+++ dcrm/web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
 
@@ -27,6 +28,26 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out")
     return redirect('home')
+
+def register(request):
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully Registered")
+            return redirect('home')
+
+    else: 
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    
+    return render(request, 'register.html', {'form': form})
 
 
 
