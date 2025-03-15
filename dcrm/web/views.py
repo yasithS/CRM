33c7b419692aeda88ff18cc50a66ff32a033e77a
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2025-03-15 08:12:48 +0530

    feat: logout feature implemented

--- dcrm/web/views.py
+++ dcrm/web/views.py
@@ -23,12 +23,10 @@ def home(request):
         return render(request, 'home.html', {})
 
 
-
-
-
-
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out")
+    return redirect('home')
 
 
 
