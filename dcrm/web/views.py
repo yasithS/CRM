ae061b1a85547f0d625a6be5401c3adf0b00ce64
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2025-03-15 12:30:12 +0530

    feat: individual records implemented

--- dcrm/web/views.py
+++ dcrm/web/views.py
@@ -54,5 +54,19 @@ def register(request):
     return render(request, 'register.html', {'form': form})
 
 
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # look for records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    
+    else:
+        messages.success(request, "You must be logged in to view records. ")
+        return redirect('home')
 
-# Create your views here.
+    
+
+
+
+
+
