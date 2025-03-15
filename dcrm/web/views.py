a21c5169926feff6f4e028946f0a7151f6189bc8
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2025-03-15 10:26:06 +0530

    feat: rocords viewer implemented

--- dcrm/web/views.py
+++ dcrm/web/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+
+    records = Record.objects.all()
 
     #Check to see if logging in
     if request.method == "POST":
@@ -20,8 +23,9 @@ def home(request):
         else:
             messages.success(request, "There was an error login in, Please try again")
             return redirect('home')
+        
     else: 
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 def logout_user(request):
